1fa4bc46e6eb51474652d412f9d3a4a0848e1fcd
HNephilim <erickdpiva@gmail.com>
2021-12-29 16:03:39 -0300

    updating

---
+++ src/configuration.rs
@@ -0,0 +1,34 @@
+
+#[derive(serde::Deserialize)]
+pub struct Settings{
+    pub database: DatabaseSettings,
+    pub app_port: u16,
+}
+
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings{
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub db_name: String,
+}
+
+impl DatabaseSettings{
+    pub fn connection_string(&self) -> String{
+        format!("postgres://{}:{}@{}:{}/{}",
+        self.username, self.password, self.host, self.port, self.db_name)
+    }
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError>{
+    //Get a default, or a reader, config
+    let mut settings = config::Config::default();
+
+    //read and parse the configuration file "config"
+    settings.merge(config::File::with_name("config"));
+
+    //convert settings in our setting
+    settings.try_into()
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use backend_project::{startup, configuration};
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let configuration = configuration::get_configuration().expect("Failed to read configuration");
+
+    let address = format!("127.0.0.1:{}", configuration.app_port);
+    let listener = std::net::TcpListener::bind(address)?;
+    println!("Port = {}", &listener.local_addr().unwrap().port());
+    startup::run_server(listener)?.await
 }
