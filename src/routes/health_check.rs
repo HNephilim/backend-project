4764d521b49669e99ace83624d191670bfc79246
HNephilim <erickdpiva@gmail.com>
2022-01-14 17:02:33 -0300

    Testing GC CI =)

--- src/routes/health_check.rs
+++ src/routes/health_check.rs
@@ -1,5 +1,5 @@
 use actix_web::HttpResponse;
 
 pub async fn health_check() -> HttpResponse{
-    HttpResponse::Ok().body("I'm working <3").finish()
+    HttpResponse::Ok().body("I'm working <3")
 }
